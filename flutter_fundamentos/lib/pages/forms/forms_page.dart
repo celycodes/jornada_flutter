c3017ec025b870fc8b3fa670024d0a538b2e7df2
celenny <celennycs@gmail.com>
2023-06-01 12:10:59 -0300

    forms

--- flutter_fundamentos/lib/pages/forms/forms_page.dart
+++ flutter_fundamentos/lib/pages/forms/forms_page.dart
@@ -24,42 +24,51 @@ class _FormsPageState extends State<FormsPage> {
       appBar: AppBar(
         title: const Text('Forms'),
       ),
-      body: Form(
-        key: formKey,
-        child: Padding(
-          padding: EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              TextFormField(
-                controller: nameEC,
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                validator: (String? value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Campo não preenchido';
-                  }
-                  return null;
-                },
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  var formValid = formKey.currentState?.validate() ?? false;
-                  var message = 'Formulário inválido';
-                  Color backgroundColorSnackBar = Colors.red;
-                  if (formValid) {
-                    message =
-                        'Obrigada por preecher o formulário ${nameEC.text}';
-                    backgroundColorSnackBar = Colors.green;
-                  }
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      content: Text(message),
-                      backgroundColor: backgroundColorSnackBar,
+      body: SingleChildScrollView(
+        child: Form(
+          key: formKey,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: nameEC,
+                  maxLines: null,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  decoration: InputDecoration(
+                    labelText: 'Nome Completo',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
                     ),
-                  );
-                },
-                child: const Text('Salvar'),
-              ),
-            ],
+                  ),
+                  validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Campo não preenchido';
+                    }
+                    return null;
+                  },
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    var formValid = formKey.currentState?.validate() ?? false;
+                    var message = 'Formulário inválido';
+                    Color backgroundColorSnackBar = Colors.red;
+                    if (formValid) {
+                      message =
+                          'Obrigada por preecher o formulário ${nameEC.text}';
+                      backgroundColorSnackBar = Colors.green;
+                    }
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text(message),
+                        backgroundColor: backgroundColorSnackBar,
+                      ),
+                    );
+                  },
+                  child: const Text('Salvar'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
