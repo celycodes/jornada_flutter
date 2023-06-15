8d48101d4b8bb9b92aaa4073bccde32b1ffff9ee
celenny <celennycs@gmail.com>
2023-06-15 11:08:05 -0300

    flutter gerencia de estado

---
+++ b_flutter_gerencia_de_estado/lib/home_page.dart
@@ -0,0 +1,46 @@
+import 'package:b_flutter_gerencia_de_estado/setState/imc_setState_page.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  void _goToPage(BuildContext context, Widget page) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => page,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () => _goToPage(context, const ImcSetStatePage()),
+              child: const Text('SetState'),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('ValueNotifier'),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('ChangeNotifier'),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Bloc Pattern (streams)'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
