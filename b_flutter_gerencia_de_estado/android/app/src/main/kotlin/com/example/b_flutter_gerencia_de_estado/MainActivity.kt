fa963207c00416c5013c68c3d2c382ce8178bbb5
celenny <celennycs@gmail.com>
2023-06-26 16:52:37 -0300

    jornada flutter

--- b_flutter_gerencia_de_estado/android/app/src/main/kotlin/com/example/b_flutter_gerencia_de_estado/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.b_flutter_gerencia_de_estado
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- b_flutter_gerencia_de_estado/lib/bloc_pattern/imc_state.dart
+++
@@ -1,6 +0,0 @@
-class ImcState {
-  final double? imc;
-  ImcState({this.imc});
-}
-
-class ImcStateLoading extends ImcState {}
