ac536df89c03d15acd64d3d31811e24ecc8c4a7b
celenny <celennycs@gmail.com>
2023-08-02 10:11:06 -0300

    database sqlite

--- c_flutter_storages/lib/database/database_sql.dart
+++ c_flutter_storages/lib/database/database_sql.dart
@@ -1,6 +1,8 @@
+// ignore_for_file: avoid_print
+
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
-import 'package:sqflite/sqlite_api.dart';
+//import 'package:sqflite/sqlite_api.dart';
 
 class DatabaseSqLite {
   //Future<Database> openConnection() async {
@@ -9,13 +11,53 @@ class DatabaseSqLite {
     final databaseFinalPath = join(databasePath, 'SQLITE_EXAMPLE');
     await openDatabase(
       databaseFinalPath,
-      version: 1,
+      version: 2,
+      onConfigure: (db) async {
+        print('onConfigure sendo chamado');
+        await db.execute('PRAGMA foreing_keys = ON');
+      },
       // onCreate -> chamado somente no momento de criação do banco de dados (na 1° vez que carrega o app)
-      onCreate: (Database db, int version) {},
+      onCreate: (Database db, int version) {
+        print('onCreate chamado');
+        final batch = db.batch();
+        batch.execute('''
+        create table teste(id Integer primary key autoincrement,
+        nome varcahar(100) 
+        )
+        ''');
+        batch.commit();
+      },
       // onUpgrade -> será chamado sempre que houver uma alteração no verison (incrementa [1 -> 2])
-      onUpgrade: (Database db, int oldVersion,int version) {},
+      onUpgrade: (Database db, int oldVersion, int version) {
+        print('onUpgrade chamado');
+        final batch = db.batch();
+        if (oldVersion == 1) {
+          batch.execute('''
+          create table produto(id Integer primary key autoincrement,
+          nome varcahar(100) 
+          )
+          ''');
+        }
+        if (oldVersion == 2) {
+          batch.execute('''
+          create table categoria(id Integer primary key autoincrement,
+          nome varcahar(100) 
+          )
+          ''');
+        }
+        batch.commit();
+      },
       // onDowngrade -> será chamado sempre que houver uma alteração no verison para menos (decrementa [2 -> 1])
-      onDowngrade: (Database db, int oldVersion,int version) {},
+      onDowngrade: (Database db, int oldVersion, int version) {
+        print('onDowngrade chamado');
+        final batch = db.batch();
+        if (oldVersion == 3) {
+          batch.execute('''
+          drop table categoria
+          ''');
+        }
+        batch.commit();
+      },
     );
   }
 }
